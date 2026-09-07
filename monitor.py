403cfd6f1250e1f3187b7272c80227b326aaf4b1
allen <linlu_2021@qq.com>
2026-09-07 22:14:55 +0800

    add disk alert

--- monitor.py
+++ monitor.py
@@ -18,8 +18,19 @@ def write_log():
 
     cpu = run_command("nproc")
     memory = run_command("free -h | awk '/Mem:/ {print $3 \"/\" $2}'")
-    disk = run_command("df -h / | awk 'NR==2 {print $5}'")
+    disk = run_command("df / | awk 'NR==2 {print $5}'")
     load = run_command("uptime | awk -F'load average:' '{print $2}'")
+
+    # 去掉磁盘使用率的 %
+    disk_number = int(disk.replace("%", ""))
+
+    # 判断系统状态
+    if disk_number >= 90:
+        status = "CRITICAL - 磁盘空间严重不足"
+    elif disk_number >= 80:
+        status = "WARNING - 磁盘空间不足"
+    else:
+        status = "正常"
 
     with open("monitor.log", "a") as f:
         f.write("=" * 40 + "\n")
@@ -28,7 +39,13 @@ def write_log():
         f.write(f"内存使用：{memory}\n")
         f.write(f"磁盘使用率：{disk}\n")
         f.write(f"系统负载：{load}\n")
-
+        f.write(f"状态：{status}\n")
+    # 如果异常，写入告警日志
+    if disk_number >= 80:
+        with open("alert.log", "a") as f:
+            f.write(
+                f"[{time_now}] {status}，当前使用率：{disk}\n"
+            )
 
 while True:
     write_log()
